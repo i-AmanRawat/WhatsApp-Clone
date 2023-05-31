5f9582c4fe582563c0b4703c730c032414fe7ad7
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2023-05-31 12:11:05 +0530

    starting conversation

--- src/components/chat/chat/ChatBox.jsx
+++ src/components/chat/chat/ChatBox.jsx
@@ -2,15 +2,27 @@ import ChatFooter from "./ChatFooter";
 import ChatHeader from "./ChatHeader";
 import ChatMessages from "./ChatMessages";
 import { AccountContext } from "../../../context/AccountProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function ChatBox() {
-  const { person } = useContext(AccountContext);
+  const { account, person } = useContext(AccountContext);
+  const [conversation, setConversation] = useState({});
+
+  useEffect(() => {
+    const getConversationDetails = async () => {
+      let data = await getConversation({
+        senderId: account.email,
+        receiverId: person.email,
+      });
+      setConversation(data);
+    };
+    getConversationDetails();
+  }, [person.email]);
 
   return (
     <>
       <ChatHeader person={person} />
-      <ChatMessages person={person} />
+      <ChatMessages person={person} conversation={conversation} />
       {/* <ChatFooter /> */}
     </>
   );
--- src/components/chat/chat/ChatFooter.jsx
+++ src/components/chat/chat/ChatFooter.jsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 
-export default function ChatFooter() {
+export default function ChatFooter({ textMsg, setTextMsg, sendText }) {
   return (
     <div className="chat-footer w-auto ">
       <footer className="flex items-center bg-[#1f2c33] py-1 px-6 h-[57px] ">
@@ -31,6 +32,9 @@ export default function ChatFooter() {
             type="text"
             placeholder="Type a message"
             className="line-height rounded-lg bg-[#2a3942] w-full px-6 text-base text-[#d1d7db]"
+            value={textMsg}
+            onChange={(e) => setTextMsg(e.target.value)}
+            onKeyPress={(e) => sendText(e)}
           />
         </div>
         <div className="ml-auto pt-1 pl-3  ">
