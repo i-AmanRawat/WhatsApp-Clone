a953a72956c9b8da136541f35ad8ff8350ad5393
Prodev <129268721+PRANJALRANA11@users.noreply.github.com>
2023-07-12 13:10:04 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io=new Server(9000,{
 	cors:{
-		origin:'https://bejewelled-moxie-ea9b9d.netlify.app/'
+		origin:'https://chatu-chatu.netlify.app/'
 		}
 	})
 
